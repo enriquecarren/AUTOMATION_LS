513b57e77a91f5a75cbb3b0b1cc94ff05a3ca3f3
Enrique Carreno <enrique.carreno@lightninstep.com>
2024-10-28 16:48:20 -0500

    Inquiry Profile Suite

---
+++ Test Suites/LS/Inquiry Profile/Inquiry Profile.ts
@@ -0,0 +1,42 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Inquiry Profile</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>1c9e51e4-571d-4a52-af84-812b8895d6cb</testSuiteGuid>
+   <testCaseLink>
+      <guid>99ef6a7e-9741-4960-b8e1-445744f81755</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Specific Cases/LS/Inquiry Profile/Demographics</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>f09e7143-48c7-4733-8f8d-b000e5a0759c</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Specific Cases/LS/Inquiry Profile/Inquiry Profile</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>1cbbc79c-d79c-42ad-8f15-77bd98bd14cb</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Specific Cases/LS/Inquiry Profile/Referral Source</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>f3afe058-07d6-40b6-a24e-4aaeb710b2f6</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Specific Cases/LS/Inquiry Profile/Scheduling</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
