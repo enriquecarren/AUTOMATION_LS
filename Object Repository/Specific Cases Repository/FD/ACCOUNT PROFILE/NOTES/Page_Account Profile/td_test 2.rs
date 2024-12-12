bd415dd177235aa3da49f3c6add53d51e82bffae
Marco Zurita <marco.zurita@lightningstep.com>
2024-12-12 17:52:34 -0500

    update notes

---
+++ Object Repository/Specific Cases Repository/FD/ACCOUNT PROFILE/NOTES/Page_Account Profile/td_test 2.rs
@@ -0,0 +1,114 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>td_test 2</name>
+   <tag></tag>
+   <elementGuidId>de00de33-21c7-4f52-85ed-a6d010141641</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>CSS</key>
+         <value>#tblAccountingNotes > tbody > tr.odd > td:nth-of-type(2)</value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//table[@id='tblAccountingNotes']/tbody/tr/td[2]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <smartLocatorCollection>
+      <entry>
+         <key>SMART_LOCATOR</key>
+         <value>internal:role=gridcell[name=&quot;test 2&quot;i] >> nth=0</value>
+      </entry>
+   </smartLocatorCollection>
+   <smartLocatorEnabled>false</smartLocatorEnabled>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>td</value>
+      <webElementGuid>a95e5ff6-3f2d-4d9e-a045-037c8518699c</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>test 2</value>
+      <webElementGuid>718e0c8b-dd53-43b3-91ae-efeee36a9c12</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;tblAccountingNotes&quot;)/tbody[1]/tr[@class=&quot;odd&quot;]/td[2]</value>
+      <webElementGuid>1bd4a4ee-c4c8-42d0-ad74-30a605d97fb4</webElementGuid>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:idRelative</name>
+      <type>Main</type>
+      <value>//table[@id='tblAccountingNotes']/tbody/tr/td[2]</value>
+      <webElementGuid>63eba72e-1321-4589-8029-afd7b91b4953</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Accounting Note'])[1]/following::td[1]</value>
+      <webElementGuid>30e12c01-41fa-4009-bce6-3d40a683e1f2</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Created on'])[1]/following::td[2]</value>
+      <webElementGuid>ac86c226-d3f1-4de3-853b-906eb1bbe4d1</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Automation Test'])[3]/preceding::td[1]</value>
+      <webElementGuid>e047cb43-8d31-4029-a301-9c28af72e36a</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Accounting Note'])[2]/preceding::td[5]</value>
+      <webElementGuid>223254d4-8652-4ce8-b79e-d53305367ba8</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>//*/text()[normalize-space(.)='test 2']/parent::*</value>
+      <webElementGuid>f13d5eea-70a7-43fa-b2f1-f5730491f983</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//div[13]/div[2]/div/div/table/tbody/tr/td[2]</value>
+      <webElementGuid>b60b8812-8090-4c42-bfd9-abb2b85e1a0b</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:customAttributes</name>
+      <type>Main</type>
+      <value>//td[(text() = 'test 2' or . = 'test 2')]</value>
+      <webElementGuid>ea21d0d5-2c31-4c4d-a830-87a4b5c9ca5f</webElementGuid>
+   </webElementXpaths>
+</WebElementEntity>
