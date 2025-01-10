c057b44c1bdb21069c5f0af7100a8c4af63f8338
Marco Zurita <marco.zurita@lightningstep.com>
2025-01-10 12:57:57 -0500

    estimates Client profile 01102025

---
+++ Object Repository/Specific Cases Repository/FD/client profile estimates creation/Page_Estimates/div_chevron_left            homepage       _3fa348.rs
@@ -0,0 +1,123 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>div_chevron_left            homepage       _3fa348</name>
+   <tag></tag>
+   <elementGuidId>2347da39-32a4-475b-82f6-5a5988ff338b</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>(.//*[normalize-space(text()) and normalize-space(.)='Dismiss'])[2]/following::div[2]</value>
+      </entry>
+      <entry>
+         <key>CSS</key>
+         <value>div.side-buttons</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <smartLocatorCollection>
+      <entry>
+         <key>SMART_LOCATOR</key>
+         <value>internal:text=&quot;chevron_left homepage chevron_left Back to Profile&quot;i</value>
+      </entry>
+   </smartLocatorCollection>
+   <smartLocatorEnabled>false</smartLocatorEnabled>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>div</value>
+      <webElementGuid>fa80172a-ea2e-4237-b3ea-9e6cd817314a</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>side-buttons</value>
+      <webElementGuid>d4833b4d-15e0-4485-b010-d4bb14a5c54b</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>
+        
+            chevron_left
+            homepage
+        
+                    
+                chevron_left
+                Back to Profile
+            
+               
+
+    </value>
+      <webElementGuid>b9588be6-dc47-43b1-9876-832c9f65a1f5</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>/html[@class=&quot;tablesaw-enhanced ls-light&quot;]/body[1]/div[@class=&quot;container-content ls-content-wrapper&quot;]/div[@class=&quot;side-buttons&quot;]</value>
+      <webElementGuid>60646cbf-c8f7-44d5-895f-2f5df2f8dc03</webElementGuid>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Dismiss'])[2]/following::div[2]</value>
+      <webElementGuid>16fd32c1-f742-4d3d-af05-3dc17b332651</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='About'])[3]/following::div[4]</value>
+      <webElementGuid>a7db3d6b-3448-4f62-bcfa-2539736258e0</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//div[11]/div</value>
+      <webElementGuid>4a162216-9377-4310-8321-6da619162847</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:customAttributes</name>
+      <type>Main</type>
+      <value>//div[(text() = '
+        
+            chevron_left
+            homepage
+        
+                    
+                chevron_left
+                Back to Profile
+            
+               
+
+    ' or . = '
+        
+            chevron_left
+            homepage
+        
+                    
+                chevron_left
+                Back to Profile
+            
+               
+
+    ')]</value>
+      <webElementGuid>0115c248-986d-42b8-b1e8-ec7ff5ebece3</webElementGuid>
+   </webElementXpaths>
+</WebElementEntity>
