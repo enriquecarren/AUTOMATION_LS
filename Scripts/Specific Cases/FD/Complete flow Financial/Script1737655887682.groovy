0c072111579dfbb6afbb76c9fca0e2d58e6e95c5
Marco Zurita <marco.zurita@lightningstep.com>
2025-01-27 17:01:44 -0500

    flow note and charge creation

--- Scripts/Specific Cases/FD/Complete flow Financial/Script1737655887682.groovy
+++ Scripts/Specific Cases/FD/Complete flow Financial/Script1737655887682.groovy
@@ -17,3 +17,40 @@ import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
 import internal.GlobalVariable as GlobalVariable
 import org.openqa.selenium.Keys as Keys
 
+WebUI.callTestCase(findTestCase('Common Cases/Login'), [:], FailureHandling.STOP_ON_FAILURE)
+
+WebUI.navigateToUrl('https://site17.lsapp.cloud/client/index/1558?section=notes')
+
+WebUI.click(findTestObject('Object Repository/Specific Cases Repository/FD/note generate charge/Page_Client Profile/button_New note'))
+
+WebUI.click(findTestObject('Object Repository/Specific Cases Repository/FD/note generate charge/Page_Client Profile/li_Connections Progress Note'))
+
+WebUI.click(findTestObject('Object Repository/Specific Cases Repository/FD/note generate charge/Page_Client Profile/a_Create            arrow_right_alt'))
+
+WebUI.selectOptionByValue(findTestObject('Object Repository/Specific Cases Repository/FD/note generate charge/Page_Connections Progress Note/select_-- Please select --Assessment5 Panel_3c5bd9'), 
+    '13', true)
+
+WebUI.click(findTestObject('Object Repository/Specific Cases Repository/FD/note generate charge/Page_Connections Progress Note/input_Subject_subject'))
+
+WebUI.setText(findTestObject('Object Repository/Specific Cases Repository/FD/note generate charge/Page_Connections Progress Note/input_Subject_subject'), 
+    'Test')
+
+WebUI.click(findTestObject('Object Repository/Specific Cases Repository/FD/note generate charge/Page_Connections Progress Note/input_Sign and Lock_dss'))
+
+WebUI.click(findTestObject('Object Repository/Specific Cases Repository/FD/note generate charge/Page_Connections Progress Note/button_Save'))
+
+WebUI.newTab('')
+
+WebUI.navigateToUrl('https://site17.lsapp.cloud/financialdashboard/chargesdashboard#')
+
+WebUI.click(findTestObject('Object Repository/Specific Cases Repository/FD/note generate charge/Page_Charges Dashboard/div_31.9K'))
+
+WebUI.click(findTestObject('Object Repository/Specific Cases Repository/FD/note generate charge/Page_Charges Dashboard/td_Connections Progress Note'))
+
+WebUI.verifyElementText(findTestObject('Object Repository/Specific Cases Repository/FD/note generate charge/Page_Charges Dashboard/td_Connections Progress Note'), 
+    'Connections Progress Note')
+
+WebUI.click(findTestObject('Object Repository/Specific Cases Repository/FD/note generate charge/Page_Charges Dashboard/span_Inquiry ID_check'))
+
+WebUI.click(findTestObject('Object Repository/Specific Cases Repository/FD/note generate charge/Page_Charges Dashboard/button_Approve'))
+
